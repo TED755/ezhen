9a1f275ae5a08a2f72b1f128de2b7f960a551313
Евгений Тихомиров <zhunek@MacBook-Air-Evgenij.local>
2023-09-16 16:05:21 +0300

    Отправка сообщений только в чат из которого они пришли

--- core/core.py
+++ core/core.py
@@ -1,20 +1,18 @@
 # import telebot
 import json
-from core.ezhen_settings import EZHEN_CHAT_ID, EZHEN_TOKEN
+from core.ezhen_settings import EZHEN_TOKEN
 from core.ezh_except import EzhException
 
 
 class EzhenCore():
     def __init__(self):
-        # self.ezhen = telebot.TeleBot(EZHEN_TOKEN)
         self.token = EZHEN_TOKEN
-        self.chat_id = EZHEN_CHAT_ID
+        # self.chat_id = EZHEN_CHAT_ID
         try:
             with open('core/ezhen_vocabluary.json', 'r') as vocabluary:
                 self.vocabluary = json.load(vocabluary)
         except FileNotFoundError:
             raise EzhException(message='Не найден словарь Эженя')
-        # self.start_core()
 
     def is_me(self, message: str) -> bool:
         split_string_arr = message.split(' ')
@@ -22,14 +20,3 @@ class EzhenCore():
             if my_name in split_string_arr[0].lower():
                 return True
         return False
-
-    # def start_core(self):
-    #     self.ezhen.infinity_polling(none_stop=True, interval=1)
-
-    # def send_message(self, msg: str):
-    #     self.ezhen.send_message(EZHEN_CHAT_ID, f"Здравствуйте. Я Эжень!!!"
-    #                                            f"Вы написали: {msg}")
-
-    # @self.ezhen.message_handler(func=lambda message: True)
-    # def get_text_messages(message):
-    #     request = message.text
--- ezhen_events.py
+++ ezhen_events.py
@@ -9,8 +9,11 @@ def start_life():
 
     @ezhen_bot.message_handler(func=lambda message: True)
     def get_text_messages(message):
+        user_id = message.from_user.id
+        chat_id = message.chat.id
+        # print(user_id, chat_id)
         request = message.text
         if ezhen.is_me(request):
-            ezhen_bot.send_message(ezhen.chat_id, f"вы написали {request}")
+            ezhen_bot.send_message(chat_id, f"вы написали {request}")
 
     ezhen_bot.infinity_polling(none_stop=True, interval=1)
